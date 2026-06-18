16a71226eed3a2976ba95b40a46f7a202889905c
Waldemar Smagacki <waldemar.smagacki@it9.com.pl>
2026-06-18 17:32:14 +0200

    init forge app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+﻿import ForgeUI, { render, IssuePanel, Text } from '@forge/ui';
+
+const App = () => {
+  return (
+    <Text>Cześć! To moja pierwsza działająca aplikacja w Forge!</Text>
+  );
+};
+
+export const run = render(
+  <IssuePanel>
+    <App />
+  </IssuePanel>
+);
